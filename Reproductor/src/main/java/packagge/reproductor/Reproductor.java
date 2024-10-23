13e9f2e31300a3bc43d3d2083ebbcf0de0571cd0
Roberto-Antonio-zapet <rzapetl@miumg.edu.gt>
2024-10-22 21:25:51 -0600

    Add files via upload

---
+++ Reproductor/src/main/java/packagge/reproductor/Reproductor.java
@@ -0,0 +1,16 @@
+
+package packagge.reproductor;
+
+import formulario.ventana;
+
+public class Reproductor {
+
+    public static void main(String[] args) {
+        
+        ventana ventana1 = new ventana();
+        ventana1.setVisible(true);
+        
+    
+
+    }
+}
